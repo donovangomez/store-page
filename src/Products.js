29b138e4d89cf26091c37301bb512167e7607c0c
Donovan <donovanjuniogomez@gmail.com>
2021-07-19 18:33:28 -0700

    pass products as props to Products component

--- src/Products.js
+++ src/Products.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Products = () => {
+const Products = ({ products, setProducts }) => {
   return (
     <div className="Products">
       <h2>Give me a job poasda</h2>
+      {products.map((product) => (
+        <h3>{product.title}</h3>
+      ))}
     </div>
   );
 };
